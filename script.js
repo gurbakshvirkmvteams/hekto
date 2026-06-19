fa9ace3eca86e2cd83acfc929488fc70f6330794
gurbaksh <gurbaksh.mvteams@gmail.com>
2026-06-19 11:37:38 +0530

    added background overlay layer to sideabar

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const featuredBtns = document.querySelectorAll('[data-tab-btn]');
 const sidebarBtn = document.querySelectorAll('.sidebar-btn');
 const loginOverlay = document.querySelector('.login-overlay');
 const closeLoginButton = document.querySelector('.close-login-button');
-const loginBtn = document.querySelector('.login-btn');
+const navBackground = document.querySelector('.nav-sidebar-overlay-background')
+
+const loginBtn = document.querySelector('.login-btn');  
 hamburger.addEventListener(('click'), () => {
-    navSidebar.classList.add('sidebar-active')
+    navSidebar.classList.toggle('sidebar-active')
 })
 crossIcon.addEventListener(('click'), () => {
     navSidebar.classList.remove('sidebar-active')
+})
+navBackground.addEventListener('click', ()=>{
+      navSidebar.classList.remove('sidebar-active')
 })
 sidebarBtn.forEach((btn) => {
     btn.addEventListener(('click'), () => {
@@ -50,7 +55,79 @@ closeLoginButton.addEventListener(('click'), () => {
 })
 loginOverlay.addEventListener('click', (e) => {
     const overlay = e.target.classList.contains('login-overlay');
-    if(overlay){
+    if (overlay) {
         loginOverlay.classList.remove('login-overlay-active');
     }
 })
+
+// swiper js 
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    breakpoints: {
+        576: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        }
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+const categoryswiper = new Swiper('.categoryswiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 1,      
+    spaceBetween: 20,
+    breakpoints: {
+        576: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 30, 
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        }
+    },
+
+    pagination: {
+        el: '.categoryswiper-pagination',
+        clickable: true,
+    },
+
+});
+const heroswiper = new Swiper('.heroswiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.heroswiper-pagination',
+        clickable: true,
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
